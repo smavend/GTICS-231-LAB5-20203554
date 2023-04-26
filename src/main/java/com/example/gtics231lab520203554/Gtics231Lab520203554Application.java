72f9993217ebdc0502b3f2b545cda93121bc6945
amanr <amanrique068@gmail.com>
2023-04-25 20:34:17 -0500

    primer commit

---
+++ src/main/java/com/example/gtics231lab520203554/Gtics231Lab520203554Application.java
@@ -0,0 +1,13 @@
+package com.example.gtics231lab520203554;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gtics231Lab520203554Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gtics231Lab520203554Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gtics231lab520203554/Gtics231Lab520203554ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gtics231lab520203554;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gtics231Lab520203554ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
